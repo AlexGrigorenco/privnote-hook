f72c02610cec54d5d00d9608132bd8d0c00e6ef8
AlexGrigorenco <alpsnhandy@gmail.com>
2024-01-14 19:04:25 +0200

    minimised code

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-(function(){
-    const input = document.getElementById('urlInput');
-    input.focus();
-    input.addEventListener('input', () => {
-        window.open(input.value.replace(/\s/g, ''), '_blank');
-        input.value = '';
-    })
-})()
+(function(){const input=document.getElementById('urlInput');input.focus();input.addEventListener('input',()=>{window.open(input.value.replace(/\s/g,''),'_blank');input.value='';})})()
